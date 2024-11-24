36ba69933e6f9418fd80ffd6d102c6b4480348c3
Filipe <hipolitofilipe@hotmail.com>
2024-11-24 11:43:17 -0300

    feat: nova permissão para manipulação de posts

--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer, PostDetailSerializer
 from .models import Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.prefetch_related("replies")
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def get_serializer(self, *args, **kwargs):
         if self.action in ["retrieve", "update", "partial_update"]:
